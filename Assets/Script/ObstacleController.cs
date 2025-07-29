899aa4db818078facd5621760a212ff612d1ceee
nakyung71 <nakyung71@naver.com>
2025-07-29 16:05:18 +0900

    장애물 기본 구조 완료

--- Assets/Script/ObstacleController.cs
+++ Assets/Script/ObstacleController.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class ObstacleController : MonoBehaviour
 {
-    Obstacles obstacles;
-    float obstacleInterval = 3f;
+
+    [SerializeField] GameObject smallDownObstaclePrefab;
+    [SerializeField] GameObject bigDownObstaclePrefab;
+    [SerializeField] GameObject smallUpObstaclePrefab;
+    [SerializeField] GameObject bigUpObstaclePrefab;
+
+    float obstacleInterval = 1f;
 
 
 
     Queue<int> objectSpawnData = new Queue<int>();
+
+    private void Start()
+    {
+        AddData();
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(SetObstacles());
+        }
+    }
 
 
     //시험용으로 
@@ -19,30 +38,58 @@ public class ObstacleController : MonoBehaviour
         objectSpawnData.Enqueue(0);
         objectSpawnData.Enqueue(1);
         objectSpawnData.Enqueue(2);
+        objectSpawnData.Enqueue(1);
+        objectSpawnData.Enqueue(4);
+        objectSpawnData.Enqueue(0);
+        objectSpawnData.Enqueue(1);
+        objectSpawnData.Enqueue(3);
+
     }
 
-    void SetObstacles()
+    IEnumerator SetObstacles()
     {
 
         //특정 간격으로 오브젝트에 대한 정보를 끌어오고 싶어
         //그럼 코루틴을 사용해서 꺼내올까?
         foreach (int data in objectSpawnData)
         {
-            if (data == 0)
-            {
-                obstacles.SetType(0);
-            }
-            else if(data == 1) 
-            {
-                obstacles.SetType(1);
-            }
-            else
-            {
-                obstacles.SetType(2);
-            }
-
-
+            SetType(data);
+            yield return new WaitForSeconds(obstacleInterval);
         }
     }
-    
+
+    void SpawnDownObstacles(GameObject downPrefab)
+    {
+        Instantiate(downPrefab, new Vector3(7f, 0f, 0f), Quaternion.identity);
+    }
+    void SpawnUpObstacles(GameObject upPrefab)
+    {
+        Instantiate(upPrefab, new Vector3(7f, 1f, 0f), Quaternion.identity);
+    }
+
+    public void SetType(int type)
+    {
+        if (type == 0)
+        {
+
+        }
+        else if (type == 1)
+        {
+            SpawnDownObstacles(smallDownObstaclePrefab);
+        }
+        else if (type == 2)
+        {
+            SpawnDownObstacles(bigDownObstaclePrefab);
+        }
+        else if (type == 3)
+        {
+            SpawnUpObstacles(smallUpObstaclePrefab);
+        }
+        else
+        {
+            SpawnUpObstacles(bigUpObstaclePrefab);
+        }
+
+    }
+
 }

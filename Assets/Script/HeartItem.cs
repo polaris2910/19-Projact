96b151da1f3cbf4b8b51f7fe575528815f84111e
polaris2910 <sswgame2910@gmail.com>
2025-07-30 19:34:52 +0900

    업적 시스템 기초

--- Assets/Script/HeartItem.cs
+++ Assets/Script/HeartItem.cs
@@ -2,25 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class HeartItem : MonoBehaviour
-//{
-//    ResourceManarger resourceManarger;
-//    public float healAmount = 20f;
+public class HeartItem : MonoBehaviour
+{
 
-//    private void OnTriggerEnter2D(Collider2D collision)
-//    {
-//        if (collision.CompareTag("Player"))
-//        {
-//            resourceManarger rc = collision.GetComponent<ResourceController>();
-//            if (rc != null)
-//            {
-//                bool healed = rc.ChangeHealth(healAmount);
-//                if (healed)
-//                {
-//                    Debug.Log("체력 회복!");
-//                    gameObject.SetActive(false);
-//                }
-//            }
-//        }
-//    }
-//}
+    public float healAmount = 20f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            ResourceManager rc = collision.GetComponent<ResourceManager>();
+            if (rc != null)
+            {
+                bool healed = rc.ChangeHealth(healAmount);
+                if (healed)
+                {
+                    Debug.Log("체력 회복!");
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}

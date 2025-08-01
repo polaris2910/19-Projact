18a3d548fbe434c4bdb503fdc47fcf6a3bc43b91
polaris2910 <sswgame2910@gmail.com>
2025-08-01 14:53:59 +0900

    업적 UI출현

--- Assets/Script/UI/UIManager.cs
+++ Assets/Script/UI/UIManager.cs
@@ -14,9 +14,9 @@ public enum UIState
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance { get; private set; }
+    public CanvasGroup achievementImageGroup;
 
 
-   
     [SerializeField] private Slider healthBar;
     
    
@@ -103,5 +103,33 @@ public class UIManager : MonoBehaviour
 
 
     }
+    public void ShowAchievementUI()
+    {
+        StartCoroutine(FadeAchievement());
+    }
 
+    private IEnumerator FadeAchievement()
+    {
+        achievementImageGroup.gameObject.SetActive(true);
+
+        // 페이드 인
+        float duration = 1f;
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            achievementImageGroup.alpha = Mathf.Lerp(0, 1, t / duration);
+            yield return null;
+        }
+        achievementImageGroup.alpha = 1;
+
+        yield return new WaitForSeconds(1.5f); // 이미지가 완전히 보이는 시간
+
+        // 페이드 아웃
+        for (float t = 0; t < duration; t += Time.deltaTime)
+        {
+            achievementImageGroup.alpha = Mathf.Lerp(1, 0, t / duration);
+            yield return null;
+        }
+        achievementImageGroup.alpha = 0;
+        achievementImageGroup.gameObject.SetActive(false);
+    }
 }
